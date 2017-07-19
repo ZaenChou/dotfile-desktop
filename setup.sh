e6fffe7edc3d7d5323a7a2642d73c36729f85e76
ZaenChou <chouzaen2002@gmail.com>
2017-07-19 21:24:22 +0900

    setup

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd ${HOME}
+ln -s ${HOME}/.dotfiles/.zshrc ${HOME}/.zshrc
+ln -s ${HOME}/.dotfiles/.xprofile ${HOME}/.xprofile
+ln -s ${HOME}/.dotfiles/.xinitrc ${HOME}/.xinitrc
+ln -s ${HOME}/.dotfiles/.offlineimaprc ${HOME}/.offlineimaprc
+ln -s ${HOME}/.dotfiles/.offlineimap.py ${HOME}/.offlineimap.py
+ln -s ${HOME}/.dotfiles/.nvidia-settings ${HOME}/.nvidia-settings
+ln -s ${HOME}/.dotfiles/.msmtprc ${HOME}/.msmtprc
+ln -s ${HOME}/.dotfiles/.bashrc ${HOME}/.bashrc
+ln -s ${HOME}/.dotfiles/.bash_profile ${HOME}/.bash_profile
