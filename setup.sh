3b66fba9ef41513a55ea2d1d5773b41093dc5ad6
ZaenChou <chouzaen2002@gmail.com>
2017-07-19 21:32:41 +0900

    first commit

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 cd ${HOME}
+ln -s ${HOME}/.dotfiles/.zprofile ${HOME}/.zprofile
 ln -s ${HOME}/.dotfiles/.zshrc ${HOME}/.zshrc
 ln -s ${HOME}/.dotfiles/.xprofile ${HOME}/.xprofile
 ln -s ${HOME}/.dotfiles/.xinitrc ${HOME}/.xinitrc
 ln -s ${HOME}/.dotfiles/.offlineimaprc ${HOME}/.offlineimaprc
 ln -s ${HOME}/.dotfiles/.offlineimap.py ${HOME}/.offlineimap.py
-ln -s ${HOME}/.dotfiles/.nvidia-settings ${HOME}/.nvidia-settings
+ln -s ${HOME}/.dotfiles/.nvidia-settings-rc ${HOME}/.nvidia-settings-rc
 ln -s ${HOME}/.dotfiles/.msmtprc ${HOME}/.msmtprc
 ln -s ${HOME}/.dotfiles/.bashrc ${HOME}/.bashrc
 ln -s ${HOME}/.dotfiles/.bash_profile ${HOME}/.bash_profile
+ln -s ${HOME}/.dotfiles/.profile ${HOME}/.profile
