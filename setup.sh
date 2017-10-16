7093db06ffb780acf10cc998319044fe01bba0b4
zaen323 <chouzaen2002@gmail.com>
2017-10-16 17:34:34 +0900

    see diff

--- setup.sh
+++ setup.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-cd ${HOME}
-ln -s ${HOME}/.dotfiles/.zprofile ${HOME}/.zprofile
-ln -s ${HOME}/.dotfiles/.zshrc ${HOME}/.zshrc
-ln -s ${HOME}/.dotfiles/.xprofile ${HOME}/.xprofile
-ln -s ${HOME}/.dotfiles/.xinitrc ${HOME}/.xinitrc
-ln -s ${HOME}/.dotfiles/.offlineimaprc ${HOME}/.offlineimaprc
-ln -s ${HOME}/.dotfiles/.offlineimap.py ${HOME}/.offlineimap.py
-ln -s ${HOME}/.dotfiles/.nvidia-settings-rc ${HOME}/.nvidia-settings-rc
-ln -s ${HOME}/.dotfiles/.msmtprc ${HOME}/.msmtprc
-ln -s ${HOME}/.dotfiles/.bashrc ${HOME}/.bashrc
-ln -s ${HOME}/.dotfiles/.bash_profile ${HOME}/.bash_profile
-ln -s ${HOME}/.dotfiles/.profile ${HOME}/.profile
-ln -s ${HOME}/.dotfiles/.spacemacs ${HOME}/.spacemacs
+ln -s .zprofile ${HOME}/.zprofile
+ln -s .zshrc ${HOME}/.zshrc
+ln -s .xprofile ${HOME}/.xprofile
+ln -s .xinitrc ${HOME}/.xinitrc
+ln -s .offlineimaprc ${HOME}/.offlineimaprc
+ln -s .offlineimap.py ${HOME}/.offlineimap.py
+ln -s .msmtprc ${HOME}/.msmtprc
+ln -s .bashrc ${HOME}/.bashrc
+ln -s .bash_profile ${HOME}/.bash_profile
+ln -s .profile ${HOME}/.profile
+ln -s .spacemacs.d ${HOME}/.spacemacs.d
