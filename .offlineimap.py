2b07fe1dca76abce9e6fd292b410b9e61b3b10d8
zaen323 <chouzaen2002@gmail.com>
2018-01-03 00:14:59 +0900

    a

--- .offlineimap.py
+++ .offlineimap.py
@@ -1,9 +1,1 @@
-import re
-import os
-import subprocess
-
-def mailpasswd(acct):
-    path = "/home/zaen/.passwd/%s.gpg" % acct
-    return subprocess.check_output(["gpg", "--quiet", "--batch", "-d", path]).strip()
-
-
+import keyring
